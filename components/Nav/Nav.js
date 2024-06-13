a5a08703f1ea7dc4170cce426d712995c7e8bb15
ericlin2 <ericlin45209@gmail.com>
2024-06-13 23:08:10 +0800

    modified globals.css, set up components + pages routing

---
+++ components/Nav/Nav.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose, AiOutlineMail } from "react-icons/ai";
+import Link from "next/link";
+
+const Nav = () => {
+  return <div>nav</div>;
+};
+
+export default Nav;
