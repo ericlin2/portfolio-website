d3db660982ed70c67fde65fd2c51bbba25204cb8
ericlin2 <ericlin45209@gmail.com>
2024-07-04 22:16:51 +0800

    implemented hero section

---
+++ components/Hero/Hero.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+import Typewriter from "typewriter-effect";
+import { motion } from "framer-motion";
+const Hero = () => {
+  return (
+    <>
+      <motion.div
+        initial={{ opacity: 0, y: 15 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 15 }}
+        transition={{ delay: 0.25 }}
+      >
+        <div className="w-full h-screen text-center">
+          <div className="flex justify-center items-center p-3 max-w-[1240px] w-full h-full mx-auto">
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ y: [-50, 0], opacity: 3 }}
+            >
+              <h1 className="  font-extrabold text-transparent text-6xl  bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                Hello, I am Eric
+              </h1>
+              <h2 className="text-gray-600  py-3">
+                <Typewriter
+                  options={{
+                    autoStart: true,
+                    loop: true,
+                  }}
+                  onInit={(typewriter) => {
+                    typewriter
+
+                      .typeString("A Full-stack Developer")
+                      .pauseFor(1000)
+                      .deleteAll()
+                      .typeString("I love to code.")
+                      .pauseFor(1000)
+                      .deleteAll()
+                      .typeString("I love to learn new technologies.")
+
+                      .start();
+                  }}
+                />
+              </h2>
+
+              <div className="flex justify-between items-center m-auto max-w-[280px] py-5">
+                <div className="rounded-full shadow-lg shadow-gray-400 p-5 cursor-pointer hover:scale-110 ease-in duration-300">
+                  <a
+                    href="https://github.com/ericlin2"
+                    target={"_blank"}
+                    rel={"noreferrer"}
+                  >
+                    <FaGithub />
+                  </a>
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-5 cursor-pointer hover:scale-110 ease-in duration-300">
+                  <a
+                    href="https://www.linkedin.com/in/ericlincmu/"
+                    target={"_blank"}
+                    rel={"noreferrer"}
+                  >
+                    <FaLinkedinIn />
+                  </a>
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-5 cursor-pointer hover:scale-110 ease-in duration-300">
+                  <a
+                    href="https://leetcode.com/ericlin45209/"
+                    target={"_blank"}
+                    rel={"noreferrer"}
+                  >
+                    <SiLeetcode />
+                  </a>
+                </div>
+                <div
+                  onClick={() =>
+                    (window.location.href = "mailto:ericlin45209@gmail.com")
+                  }
+                  className="rounded-full shadow-lg shadow-gray-400 p-5 cursor-pointer hover:scale-110 ease-in duration-300"
+                >
+                  <AiOutlineMail />
+                </div>
+              </div>
+            </motion.div>
+          </div>
+        </div>
+      </motion.div>
+    </>
+  );
+};
+
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
+import Head from "next/head";
 import Nav from "@/components/Nav/Nav";
+import Hero from "@/components/Hero/Hero";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -7,7 +9,12 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Eric | Full Stack Developer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Nav />
+      <Hero />
     </>
   );
 }
